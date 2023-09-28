28a4fa7be53094b4ceccb99a93e71ef35d80b28c
Pieter Smit <pieter.smit@twgroup.co.nz>
2023-09-28 23:08:25 +1300

    fix: verbose output

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,22 @@ mod write_file;
 
 fn main() -> io::Result<()> {
     let args = args::Args::parse();
-    println!("{:#?}", args);
-    // for _ in 0..len(args) {
-    //     println!("Hello {}!", args.name)
-    // }
-    // Call the read function from read_file module
+    if args.verbose {
+        eprintln!("{:?}", prog());
+        eprintln!("{:#?}", args);
+    }
     let lines = read_file::read(&args.src)?;
     let updated_lines =
         search_replace::update(&args.start, &args.end, &args.replace, lines, args.verbose);
     write_file::write_to(&args.dst, updated_lines)?;
     Ok(())
+}
+
+fn prog() -> Option<String> {
+    std::env::current_exe()
+        .ok()?
+        .file_name()?
+        .to_str()?
+        .to_owned()
+        .into()
 }
